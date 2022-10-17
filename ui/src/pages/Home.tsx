13c1b554ee99cc1e8f4896fedd089512156d579b
Vladimir Nesterov <46971653+t67y110v@users.noreply.github.com>
2022-10-18 01:56:09 +0400

    Обновление главной страницы

--- ui/src/pages/Home.tsx
+++ ui/src/pages/Home.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect } from 'react';
+import { Modal, Button, Group, TextInput, PasswordInput } from "@mantine/core"
+import { IconLogin, IconLock, IconAt } from '@tabler/icons';
 import './pages.css'
 
 const Home = (props: { name: string }) => {
@@ -22,19 +24,32 @@ const Home = (props: { name: string }) => {
 
     return (
 
-        <div className="container">
-            <div className="row">
-                <div className="col-2"></div>
-                <div className="col-8">
+        <div className="container-fluid info-form-container">
+            <div className="row space-around">
+                
+                <div className="col-5 info-container">
+                    <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus voluptatum nam placeat sequi exercitationem, cum consequuntur nulla dicta. At ullam expedita dolor culpa consectetur laudantium fugit quisquam earum, ipsa commodi.</h1>
+                    <Button leftIcon={<IconLogin />} fullWidth size="md" variant="light" className='btn-cr' color="cyan" radius="xl" type="submit">Использоввать</Button>
+                </div>
+                
+                <div className="col-5 info-container">
                     <div>
-                        {props.name ? 'Hi ' + props.name : 'You are not logged in'}
-                        <br></br>
+                       
                         <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis dolor voluptas omnis et, sequi provident magni numquam aliquid unde mollitia maiores fugiat, facilis nemo. Nisi voluptas in quasi exercitationem sint.</h1>
+                        <Button leftIcon={<IconLogin />} fullWidth size="md" variant="light" className='btn-cr' color="cyan" radius="xl" type="submit">Узнать больше</Button>
                     </div>
+                </div>
+                
+            </div>
+            <br></br>
+            <div className="row space-around">
+            <div className="col-10 info-container">
+                    <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus voluptatum nam placeat sequi exercitationem, cum consequuntur nulla dicta. At ullam expedita dolor culpa consectetur laudantium fugit quisquam earum, ipsa commodi.</h1>
+               
                 </div>
             </div>
 
-            
+
         </div>
 
     );
